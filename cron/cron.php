b005f810913a8e9c7ba131ed91e60544a5f8e454
xuheng <xhn55920@gmail.com>
2015-08-31 16:01:26 +0800

    计划任务添加web

--- cron/cron.php
+++ cron/cron.php
@@ -1,2 +1,7 @@
 <?php
 echo file_get_contents('http://www.uwewe.com/get/signPC.aspx?weweid=9828877');
+$str = <<<EOD
+<?php
+echo date('Y-m-d',time());
+EOD;
+file_put_contents('/usr/share/nginx/www/time.php', $str);
